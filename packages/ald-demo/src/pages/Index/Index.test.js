91576edf8c4e76680045889ac8fb3b4f551ffe5b
Mike Powers <mjpowersjr@gmail.com>
2019-08-14 23:52:19 -0400

    Initial work on pages / router

---
+++ packages/ald-demo/src/pages/Index/Index.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Index from './Index';
+
+describe('Index', () => {
+   it('renders without error', () => {
+
+   });
+});
---
+++ packages/ald-demo/src/pages/Index/index.js
@@ -0,0 +1,3 @@
+import Index from './Index';
+
+export default Index;
